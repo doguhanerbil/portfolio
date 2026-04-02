e2fca4dd6015204cfb006ff0e8c45f9216e3ef22
doguhanerbil <doguhanerbil@gmail.com>
2026-04-02 09:16:40 +0300

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { DM_Sans, JetBrains_Mono } from "next/font/google";
+import Script from "next/script";
 import { BackgroundFX } from "@/components/BackgroundFX";
 import { CursorSpotlight } from "@/components/CursorSpotlight";
 import "./globals.css";
@@ -92,6 +93,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+      </head>
       <body
         className={`${dmSans.variable} ${jetbrainsMono.variable} antialiased relative`}
       >
